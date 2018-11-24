1bcf469ae2861fe60c19985275cdd01f5c62bfe3
Gerhard Preuss <gelipp@gmail.com>
2018-11-24 11:14:38 +0100

    WIP

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 const html = String.raw
-const getIscaleUrl = (e, t, i, s) => {
+const getIscaleUrl = (e, t, i, s, dt) => {
   if (0) {
     return window.imgMinUrl
+  }
+  console.log('dt', dt)
+  if (dt) {
+    if (dt > 600) {
+      console.log(1)
+      s = 5
+    } else if (dt > 300) {
+      console.log(2)
+      s = s / 3
+    }
+    console.log(s)
   }
   return `https://img-scale.now.sh?format=${t}&width=${
     (i = Math.floor(i)) > 50 ? i - (i % 50) : i - (i % 5)
@@ -24,7 +35,7 @@ const template = html`
   <picture>
     <source type="image/webp" />
     <source type="image/jpeg" />
-    <img />
+    <img part="img" />
   </picture>
   <style>
     picture,
@@ -47,11 +58,27 @@ const template = html`
       bottom: 0;
       filter: blur(15px);
       transform: scale(1.03);
-      transition: all 0.3s linear;
+      transition: all 0.7s ease-in-out;
     }
     :host {
       display: block;
-      background: gray;
+      background: #fffaf4;
+      overflow: hidden;
+    }
+
+    :host([preview]),
+    :host([loaded]) {
+      background: none;
+    }
+
+    :host([preview]) img {
+      opacity: 1;
+    }
+
+    :host([loaded]) img {
+      opacity: 1;
+      filter: blur(0px);
+      transform: scale(1);
     }
   </style>
 `
@@ -62,94 +89,146 @@ const getPaddingTop = e => {
 }
 let postponedHighres = []
 const pendingPreviews = {}
+let isOnScrollInit
 customElements.define(
   'img-min',
   class extends HTMLElement {
     constructor() {
-      super(),
-        0 === invisibles.length &&
-          document.addEventListener('scroll', onScroll),
-        invisibles.push(this),
-        (this.maxWidth = 0)
-      const e = this.attachShadow({
+      super()
+      if (!isOnScrollInit) {
+        isOnScrollInit = true
+        document.addEventListener('scroll', onScroll)
+      }
+
+      invisibles.push(this)
+      this.maxWidth = 0
+      const shadowRoot = this.attachShadow({
         mode: 'open'
       })
-      ;(e.innerHTML = template),
-        (this.picture = e.querySelector('picture')),
-        (this.jpeg = e.querySelector('source[type="image/jpeg"]')),
-        (this.webp = e.querySelector('source[type="image/webp"]')),
-        (this.img = this.shadowRoot.querySelector('img'))
-      let t,
-        i = 0
-      ;(this.img.onload = e => {
-        ++i
-        console.log(getComputedStyle(this).borderColor)
-        this.setAttribute('preview', '')
-        console.log(getComputedStyle(this).borderColor)
-        setTimeout(() => {}, 100)
-        delete pendingPreviews[this.getAttribute('src')]
-        this.img.style.opacity = '1'
-        if (i === 2) {
-          if (Date.now() - t < 300) {
-          }
-          this.img.style.filter = 'blur(0px)'
-          this.img.style.transform = 'scale(1)'
-          this.img.onload = null
-        }
-        if (i === 1) {
-          t = Date.now()
-          postponedHighres.push(() => this.resize())
-        }
+      shadowRoot.innerHTML = template
+      this.picture = shadowRoot.querySelector('picture')
 
-        0 === Object.keys(pendingPreviews).length &&
-          (postponedHighres.reverse().forEach(e => e()),
-          (postponedHighres = []))
-      }),
-        (this.resize = this.resize.bind(this)),
-        addEventListener('resize', this.resize)
+      this.img = this.shadowRoot.querySelector('img')
+      const onHighresLoaded = () => {
+        this.removeAttribute('preview')
+        this.setAttribute('loaded', '')
+        this.img.onload = null
+      }
+      const onPreviewLoaded = e => {
+        console.log(e)
+        this.previewLoadTime = Date.now() - this.previewStartTime
+        delete pendingPreviews[this.getAttribute('src')]
+        this.setAttribute('preview', '')
+        postponedHighres.push(() => this.resize())
+        if (Object.keys(pendingPreviews).length === 0) {
+          postponedHighres.reverse().forEach(e => e())
+          postponedHighres = []
+        }
+        this.img.onload = onHighresLoaded
+      }
+      this.img.onload = onPreviewLoaded
+      this.resize = this.resize.bind(this)
+      addEventListener('resize', this.resize)
     }
-    setSrc(e) {
-      ;(this.jpeg.srcset = getIscaleUrl(
-        this.getAttribute('src'),
+
+    static get observedAttributes() {
+      return ['src', 'quality']
+    }
+
+    get src() {
+      return this.getAttribute('src')
+    }
+
+    get quality() {
+      return this.getAttribute('quality') || 50
+    }
+
+    set quality(quality) {
+      this.setAttribute('quality', quality)
+      console.log('q', quality)
+      this.updateSrcset()
+    }
+
+    set src(url) {
+      this.setAttribute('src', url)
+      this.updateSrcset()
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+      if (oldValue === newValue) return
+      this[name] = newValue
+    }
+
+    updateSrcset() {
+      if (!this.visible) {
+        return
+      }
+      this.jpeg =
+        this.jpeg || this.shadowRoot.querySelector('source[type="image/jpeg"]')
+      this.jpeg.srcset = getIscaleUrl(
+        this.src,
         'jpeg',
-        e,
-        this.getAttribute('quality') || 50
-      )),
-        (this.webp.srcset = getIscaleUrl(
-          this.getAttribute('src'),
-          'webp',
-          e,
-          this.getAttribute('quality') || 50
-        ))
+        this.width,
+        this.quality,
+        this.previewLoadTime
+      )
+      this.webp =
+        this.webp || this.shadowRoot.querySelector('source[type="image/webp"]')
+      this.webp.srcset = `${getIscaleUrl(
+        this.src,
+        'webp',
+        this.width,
+        this.quality,
+        this.previewLoadTime
+      )} 1x,${getIscaleUrl(
+        this.src,
+        'webp',
+        this.width * 1.3,
+        this.quality,
+        this.previewLoadTime
+      )} 2x`
     }
+
+    get width() {
+      return this._width
+    }
+
+    set width(width) {
+      this._width = width
+      this.updateSrcset()
+    }
+
     disconnectedCallback() {
       removeEventListener('resize', this.resize)
     }
+    loadPreview(width) {
+      pendingPreviews[this.src] = true
+      this.previewStartTime = Date.now()
+      this.width = Math.max(10, width / 7)
+      console.log('comple', this.img.complete)
+    }
     checkVisibility() {
-      const {
-          top: e,
-          bottom: t,
-          width: i,
-          height: s
-        } = this.getBoundingClientRect(),
-        r = t < -100,
-        n = e > window.innerHeight + 100
-      r ||
-        n ||
-        this.visible ||
-        ((pendingPreviews[this.getAttribute('src')] = !0),
-        this.setSrc(Math.max(10, i / 7)),
-        (this.visible = !0))
+      const dist = parseInt(this.getAttribute('lazy-dist')) || 100
+      const { top, bottom, width } = this.getBoundingClientRect()
+      const isAbove = bottom < -dist
+      const isBelow = top > window.innerHeight + dist
+      if (!isAbove && !isBelow && !this.visible) {
+        this.visible = true
+        this.loadPreview(width)
+      }
     }
     resize() {
-      this.visible &&
-        (cancelAnimationFrame(this.af),
-        (this.af = requestAnimationFrame(() => {
-          let e
-          const { width: t } = this.picture.getClientRects()[0]
-          ;(e = parseInt(t)) <= this.maxWidth ||
-            (this.setSrc(e), (this.maxWidth = e))
-        })))
+      if (!this.visible) {
+        return
+      }
+      cancelAnimationFrame(this.af)
+      this.af = requestAnimationFrame(() => {
+        const { width } = this.picture.getBoundingClientRect()
+        if (width > this.maxWidth) {
+          this.maxWidth = width
+          this.width = width
+        }
+      })
     }
     connectedCallback() {
       const e = this.getAttribute('aspect')
